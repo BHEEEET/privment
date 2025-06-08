e843c4daff694af14d8abe8656fdaaa0e649a7f6
krkbheet@gmail.com <krkbheet@gmail.com>
2025-06-08 22:24:40 +0200

    fix: depreceated images.domains next.config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,9 +1,18 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['lh3.googleusercontent.com', 'afqrpyamxardgqbhytyx.supabase.co'], // 👈 add any additional trusted domains here
-  },
+ images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'afqrpyamxardgqbhytyx.supabase.co',
+      },
+    ],
+  }, 
 }
 
 module.exports = nextConfig
