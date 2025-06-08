61cd41cd0fa7173b99452e426cd06a28d213bfc4
krkbheet@gmail.com <krkbheet@gmail.com>
2025-06-08 22:25:55 +0200

    feat: add embedded wallet layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,31 +1,71 @@
+'use client'
+
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+//import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { WalletContextProvider } from '@/contexts/WalletContext'
+import { PrivyProvider } from '@privy-io/react-auth';
+import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+import { SolanaProvider } from '@/components/SolanaProvider'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Privment',
-  description: 'Your private payment solution',
-}
+// export const metadata = {
+//   title: 'Privment',
+//   description: 'Your private payment solution',
+// }
+
+
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const supabase = createServerComponentClient({ cookies })
-  const { data: { session } } = await supabase.auth.getSession()
+  //const supabase = createServerComponentClient({ cookies })
+  //const { data: { session } } = await supabase.auth.getSession()
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <WalletContextProvider>
-          {children}
-        </WalletContextProvider>
+        <SolanaProvider>
+          <PrivyProvider
+            appId="cmbhvk3z800i6jp0ns1bgoema"
+            clientId="client-WY6MAVh7ybTtyArKkU83grLMcjLQDMA8VhN2QddZcN4CG"
+            config={{
+              appearance: {
+                accentColor: "#000000",
+                theme: "#FFFFFF",
+                showWalletLoginFirst: false,
+                logo: "https://raw.githubusercontent.com/BHEEEET/privment/refs/heads/web/web/public/logo.png",
+              },
+              loginMethods: [
+                "email",
+                "google",
+                "wallet"
+              ],
+              // Create embedded wallets for users who don't have a wallet
+              embeddedWallets: {
+                showWalletUIs: true,
+                solana: {
+                  createOnLogin: 'users-without-wallets'
+                }
+              },
+              externalWallets: {
+                solana: {
+                  connectors: toSolanaWalletConnectors()
+                }
+              },
+              mfa: {
+                "noPromptOnMfaRequired": false
+              }
+            }}
+          >
+            {children}
+          </PrivyProvider>
+        </SolanaProvider>
       </body>
     </html>
   )
